8a7781cdbe9f8ddcb4fe360857bca9edaff7b031
alejandro-cepeda <115722482+alejandro-cepeda@users.noreply.github.com>
2022-12-07 18:18:14 -0600

    Add files via upload

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, string_value, bool_value) VALUES (1, 'CRUNCH TIME BRUH', '1');
+
+--UPDATE sandbox SET int32_value = -1024620 WHERE id = 83;
+
+--DELETE FROM sandbox WHERE id = 83;
+
+SELECT * FROM sandbox;
